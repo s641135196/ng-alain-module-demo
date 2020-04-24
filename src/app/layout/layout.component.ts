c219e953264c721f11ef80f30f989b908949da23
qinzhongbao <qin_zhongbao@163.com>
2020-04-24 23:00:16 +0800

    删除多余文件

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,18 +1,11 @@
 import {
-  Component,
-  AfterViewInit
+  Component
 } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
 })
-export class layoutComponent implements AfterViewInit {
-  constructor(private router:Router){
+export class layoutComponent  {
 
-  }
-  ngAfterViewInit(): void {
-   // this.router.navigate(['/work-order/custom-info']);
-}
 }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { layoutComponent } from './layout.component';
 import { DelonABCModule } from '@delon/abc';
 import { DelonACLModule } from '@delon/acl';
 import { DelonFormModule } from '@delon/form';
 import { AlainThemeModule } from '@delon/theme';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 const COMPONENTS = [
     layoutComponent
   ];
@@ -14,8 +13,6 @@ const COMPONENTS = [
 @NgModule({
   declarations: [...COMPONENTS],
   imports: [
-    CommonModule,
-    FormsModule,
     ReactiveFormsModule,
     RouterModule,
     AlainThemeModule,
